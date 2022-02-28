e013ef0d870d45498df66c15a86e64e6f7a6594a
Qarton <mathieumarkides@gmail.com>
2022-02-28 11:07:55 +0100

    Première mise en page, ajout photos et ajouts logo

---
+++ src/components/HomePhoto.vue
@@ -0,0 +1,72 @@
+<template>
+<section id="home-photo">
+        <div class="py-12"></div>
+
+        <v-container>
+          <h2 class="display-2 font-weight-bold mb-3 text-uppercase text-center">Exemples</h2>
+
+          <v-responsive
+            class="mx-auto mb-12"
+            width="56"
+          >
+            <v-divider class="mb-1"></v-divider>
+
+            <v-divider></v-divider>
+          </v-responsive>
+
+          <v-row>
+            <v-col
+              v-for="({ src, text, title }, i) in articles"
+              :key="i"
+              cols="12"
+              md="4"
+            >
+              <v-img
+                :src="src"
+                class="mb-4"
+                height="275"
+                max-width="100%"
+              ></v-img>
+
+              <h3
+                class="font-weight-black mb-4 text-uppercase"
+                v-text="title"
+              ></h3>
+
+              <div
+                class="title font-weight-light mb-5"
+                v-text="text"
+              ></div>
+
+            </v-col>
+          </v-row>
+        </v-container>
+
+        <div class="py-12"></div>
+      </section>
+</template>
+
+<script>
+export default {
+  name: 'HomePhoto',
+  data: () => ({
+    articles: [
+          {
+            src: require('@/assets/img/img1.jpg'),
+            title: 'Photo 1',
+            text: 'Phasellus lorem enim, luctus ut velit eget, convallis egestas eros. Sed ornare ligula eget tortor tempor, quis porta tellus dictum.',
+          },
+          {
+            src: require('@/assets/img/img2.jpg'),
+            title: 'Photo 2',
+            text: 'Nam ut leo ipsum. Maecenas pretium aliquam feugiat. Aenean vel tempor est, vitae tincidunt risus. Sed sodales vestibulum nibh.',
+          },
+          {
+            src: require('@/assets/img/img3.jpg'),
+            title: 'Photo 3',
+            text: 'Vestibulum in dictum velit, in rhoncus nibh. Maecenas neque libero, interdum a dignissim in, aliquet vitae lectus. Phasellus lorem enim, luctus ut velit eget.',
+          },
+        ]
+  }),
+}
+</script>
---
+++ src/components/LandingPage.vue
@@ -0,0 +1,51 @@
+<template>
+  <section id="hero">
+    <v-parallax :height="$vuetify.breakpoint.smAndDown ? 900 : 1080" src="../assets/img/bg2.jpg">
+        <v-row no-gutters>
+            <v-theme-provider dark>
+              <v-container fill-height>
+                <v-row
+                  align="center"
+                  class="white--text mx-auto"
+                  justify="center"
+                >
+                  <v-img
+                      src="../assets/drone-w.png"
+                      max-width="50%"
+                    ></v-img>
+
+                  <v-col
+                    class="white--text text-center"
+                    cols="12"
+                    tag="h1"
+                  >
+                  <span
+                    :class="[$vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2']"
+                      class="font-weight-light"
+                    >
+                      Page en construction
+                    </span>
+
+                  </v-col>
+
+                  <v-btn
+                    class="align-self-end"
+                    fab
+                    outlined
+                    @click="$vuetify.goTo('#about-me')"
+                  >
+                    <v-icon>mdi-chevron-double-down</v-icon>
+                  </v-btn>
+                </v-row>
+              </v-container>
+            </v-theme-provider>
+        </v-row>
+    </v-parallax>
+      </section>
+</template>
+
+<script>
+export default {
+  name: 'LandingPage'
+}
+</script>
